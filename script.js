6a219a89021d41b67edc4b6580faa022787f20e3
Kakaiii21 <dellamascasey@gmail.com>
2024-06-28 02:56:10 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('spinner-container').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    }, 5000); // Adjust this delay as needed
+
+ 
+  
+    
+
+
+
+};
